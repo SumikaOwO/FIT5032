1d48da3bdfdc9ca28b912006a203918024e0d0b1
sumikaovo <sumikaowo@gmail.com>
2025-08-08 21:37:35 +1000

    Add MyName component

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="my-name">
+    <h1>Welcome to my website</h1>
+    <p>My name is <strong>{{ name }}</strong></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyName",
+  data() {
+    return {
+      name: "Yuanhao Li"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.my-name {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
+
