d4b44bac52de42d115214321bebdc1b51f5fb540
sumikaovo <sumikaowo@gmail.com>
2025-08-08 22:58:54 +1000

    Highlight a specific author

--- yli-library/src/components/JSON.vue
+++ yli-library/src/components/JSON.vue
@@ -96,6 +96,17 @@
     <section class="lab-section">
       <h2>Attribute, Class and Style Binding with <code>v-bind</code></h2>
       <p>Highlighting Specific Authors:</p>
+        <ul>
+          <li
+            v-for="author in authors"
+            :key="author.id"
+            :class="{ highlight: author.name === 'George Orwell' }"
+            :style="author.name === 'George Orwell' ? { color: 'white', backgroundColor: 'green', fontWeight: 'bold' } : {}"
+            title="Famous Author"
+          >
+            {{ author.name }} ({{ author.birthYear }})
+          </li>
+        </ul>
     </section>
   </div>
 </template>
