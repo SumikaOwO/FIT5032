dfab119e886d5a32e0ff2b4ef7656821c9571a19
sumikaovo <sumikaowo@gmail.com>
2025-08-22 23:50:33 +1000

    Week 4 P task completed (Activity 1~3)

--- yli-library/src/main.js
+++ yli-library/src/main.js
@@ -3,7 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // import './assets/main.css'
 // import './style.css'
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import TheWelcome from './components/TheWelcome.vue';
+
+const app = createApp(App);
+app.use(PrimeVue,{
+    theme: {
+        preset: Aura
+    }
+});
 
 createApp(App).mount('#app')
